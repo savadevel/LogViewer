a66ed1bd014c9d02492166fce0caeac40ad57072
Savadevel <savadevel@gmail.com>
2024-02-26 21:23:49 +0300

    extract MailLog to class singleton

--- lib/LogViewer/Utils.pm
+++ lib/LogViewer/Utils.pm
@@ -1,79 +1,44 @@
-use utf8;
 package LogViewer::Utils;
+
+# Общие функции используемые в приложении:
+# init_app - инициализация приложения (создание структуры таблиц, загрузка почтового лога)
+# is_valid_address - проверка что переданная строка в формате адреса эл. почты
+# extract_address - извлекает адреса эл. почты из переданной строки
 use strict;
 use warnings FATAL => 'all';
 
-use Exporter qw/import/;
+use parent 'Exporter';
 
 use DateTime;
 use Try::Tiny;
 
 use Email::Address;
 use LogViewer::DB;
+use LogViewer::MailLog;
 
-our @EXPORT = qw/load_log_file_to_db is_valid_address/;
-
-use constant FILE_LOG => './logs/out';
-
-use constant UNKNOWN_ADDRESS => undef;
 use constant DEFAULT_STATUS => 1;
 
-use constant FLAGS =>
-    {
-        '<='     => # прибытие сообщения (в этом случае за флагом следует адрес отправителя)
-            \&_put_to_tbl_message,
-        '=>'     => # нормальная доставка сообщения
-            \&_put_delivery_msg_to_tbl_log,
-        '->'     => # дополнительный адрес в той же доставке
-            \&_put_delivery_msg_to_tbl_log,
-        '**'     => # доставка не удалась
-            \&_put_delivery_msg_to_tbl_log,
-        '=='     => # доставка задержана (временная проблема)
-            \&_put_delivery_msg_to_tbl_log,
-        'common' => # В случаях, когда в лог пишется общая информация, флаг и адрес получателя не указываются
-            \&_put_information_msg_to_tbl_log,
-    };
+our @EXPORT = qw/extract_address init_app is_valid_address/;our @EXPORT_OK = qw/extract_address init_app is_valid_address/;
 
-sub load_log_file_to_db {
-    my $dbh = LogViewer::DB->dbh;
+# инициализация приложения (создание структуры таблиц, загрузка почтового лога)
+sub init_app {
+    my $dbh = LogViewer::DB->instance->dbh;
+    # создаем требуемую структуру объектов по ORM (auto_drop_tables не работает для PG)
+    $dbh->deploy( { auto_drop_tables => 1 } );
 
-    open(my $fh, '<', FILE_LOG) or die FILE_LOG . ' ' . $!;
-
-    while (my $line = <$fh>) {
-        chomp($line);
-
-        my ($year, $mon, $day, $hour, $min, $sec, $log) = ($line =~ /^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}) (.*?)$/);
-        my ($int_id) = ($log =~ /^([\S]{1,})/g);
-        my ($flag, $raw_address) = ($log =~ / ([\S]{2}) ([\S]{1,})/g);
-        my ($date_time) = DateTime->new(year => $year,
-            month                            => $mon,
-            day                              => $day,
-            hour                             => $hour,
-            minute                           => $min,
-            second                           => $sec,
-            time_zone                        => 'GMT',
-        );
-
-        if (defined $flag && defined FLAGS->{$flag}) {
-            FLAGS->{$flag}($dbh, $date_time, $int_id, $raw_address, $log);
-            next;
-        }
-
-        FLAGS->{common}($dbh, $date_time, $int_id, $log);
-    }
-
-    close $fh;
+    # подписка на события загрузки почтового лога,
+    # пишем в БД, при срабатывании события
+    LogViewer::MailLog->instance(
+        on_message      => \&_put_to_tbl_message,
+        on_log          => \&_put_to_tbl_log,
+        on_information  => \&_put_to_tbl_log,
+    );
 }
 
 sub _put_to_tbl_message {
-    my ($dbh, $date_time, $int_id, $raw_address, $log) = @_;
-    my $id = _extract_id($log);
-
-    return _put_delivery_msg_to_tbl_log($dbh, $date_time, $int_id, $raw_address, $log) unless (defined $id);
-
-    my $address = _extract_address($raw_address);
+    my ($mail_log, $date_time, $int_id, $id, $address, $log) = @_;
+    my $dbh = LogViewer::DB->instance->dbh;
     my $rs = $dbh->resultset('Message');
-
     try {
         $rs->create({
             created => $date_time,
@@ -89,31 +54,10 @@ sub _put_to_tbl_message {
     }
 }
 
-sub is_valid_address {
-    return defined _extract_address(shift);
-}
-
-sub _extract_address {
-    my @addresses = Email::Address->parse(shift);
-    return UNKNOWN_ADDRESS if (1 != scalar(@addresses));
-    return $addresses[0]->address;
-}
-
-sub _extract_id {
-    my ($id) = (shift =~ /id=(\S{1,})/);
-    return $id;
-}
-
-sub _put_delivery_msg_to_tbl_log {
-    my ($dbh, $date_time, $int_id, $raw_address, $log) = @_;
-    my $address = _extract_address($raw_address);
-    _put_to_tbl_log($dbh, $date_time, $int_id, $address, $log);
-}
-
 sub _put_to_tbl_log {
-    my ($dbh, $date_time, $int_id, $address, $log) = @_;
+    my ($mail_log, $date_time, $int_id, $address, $log) = @_;
+    my $dbh = LogViewer::DB->instance->dbh;
     my $rs = $dbh->resultset('Log');
-
     try {
         $rs->create({
             created => $date_time,
@@ -127,9 +71,19 @@ sub _put_to_tbl_log {
     }
 }
 
-sub _put_information_msg_to_tbl_log {
-    my ($dbh, $date_time, $int_id, $log) = @_;
-    _put_to_tbl_log($dbh, $date_time, $int_id, UNKNOWN_ADDRESS, $log);
+# проверка что переданная строка в формате адреса эл. почты
+# возвращает true если адрес в формате эл. почты
+sub is_valid_address {
+    return defined extract_address(shift);
+}
+
+# извлекает адреса эл. почты из переданной строки
+# возвращает адрес в формате эл. почты,
+# возвращает undef если стока не содержит адреса эл. почты
+sub extract_address {
+    my @addresses = Email::Address->parse(shift);
+    return undef if (1 != scalar(@addresses));
+    return $addresses[0]->address;
 }
 
 1;
